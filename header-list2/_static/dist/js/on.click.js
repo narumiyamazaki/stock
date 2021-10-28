49337ebe96d7eabc44bbbd2279452825664b5134
naruiyamazaki <flaming.june123@gmail.com>
2021-10-28 17:10:56 +0900

    20211028_1710

---
+++ header-list2/_static/dist/js/on.click.js
@@ -0,0 +1,12 @@
+/*ハンバーガーメニューのクリックイベント*/
+$('.p-nav-btn').on('click', function () {
+    $('.p-header--nav-body').toggleClass('open');
+    $('.p-nav-btn > a').toggleClass('open');
+});
+
+/*サブメニューのクリックイベント*/
+$('.p-nav--service').on('click', function () {
+    $('.p-sub-menu--service').toggleClass('open');
+    /*サブメニューを横に出すためのコード*/
+    $('.p-nav--service').toggleClass('open');
+});
