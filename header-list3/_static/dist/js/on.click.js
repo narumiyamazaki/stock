9886748caffe83dcded3e23267a85e6dcb5a65ac
naruiyamazaki <flaming.june123@gmail.com>
2022-02-05 20:30:45 +0900

    20210205

--- header-list3/_static/dist/js/on.click.js
+++ header-list3/_static/dist/js/on.click.js
@@ -1,10 +1,27 @@
 /*ハンバーガーメニューのクリックイベント*/
 $('.p-nav-btn').on('click', function () {
-    $('.p-header--nav-body').toggleClass('open');
-    $('.p-nav-btn > a').toggleClass('open');
+        $('.p-header--nav-body').toggleClass('open');
+        $('.p-nav-btn > a').toggleClass('open');
 });
 
 /*サブメニューのクリックイベント*/
-$('.p-nav--service').on('click', function () {
-    $('.p-sub-menu--service').toggleClass('open');
+
+$(function(){
+	if (window.matchMedia('(max-width: 1024px)').matches) {
+		/* ウィンドウサイズ1024px以下の処理を記述 */
+        $('.p-nav--service').on('click',function(){
+            $('.p-sub-menu--service').toggleClass('open');
+        });
+    }
 });
+
+/*
+$(function(){
+	if (window.matchMedia('(min-width: 1025px)').matches) {
+
+        $('.p-nav--service').hover(function(){
+            $('.p-sub-menu--service').toggleClass('open');
+        });
+    }
+});
+*/
