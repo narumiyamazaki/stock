ca2881048cd8410fddaecefbda11ac816709c312
unknown <flaming.june123@gmail.com>
2023-02-08 20:57:46 +0900

    20230208_2057

--- header-list4/_static/dist/js/on.click.js
+++ header-list4/_static/dist/js/on.click.js
@@ -1,7 +1,7 @@
 /*ハンバーガーメニューのクリックイベント*/
-$('.p-nav-btn').on('click', function () {
-    $('.p-header--nav-body').toggleClass('is-open');
-    $('.p-nav-btn > a').toggleClass('is-open');
+$('.p-nav__btn').on('click', function () {
+    //$('.p-header__nav-body').toggleClass('is-open');
+    $('.p-nav__btn > a').toggleClass('is-open');
     //薄い背景部分
     $('.p-background-color').toggleClass('is-show');
     $('.p-sidebar__body').toggleClass('is-show');
@@ -9,8 +9,8 @@ $('.p-nav-btn').on('click', function () {
 });
 
 /*サブメニューのクリックイベント*/
-$('.p-nav--menu').on('click', function () {
-    $('.p-nav--menu--items').toggleClass('is-open');
+$('.p-nav__item__has-sub-menu').on('click', function () {
+    $('.p-nav__sub-menu__items').toggleClass('is-open');
 });
 
 /*サイドバーを閉じるボタンのクリックイベント*/
@@ -18,5 +18,5 @@ $('.p-sidebar__btn').on('click', function () {
     $(this).children('a').toggleClass('is-open');
     $('.p-background-color').toggleClass('is-show');
     $('.p-sidebar__body').toggleClass('is-show');
-    $('.p-nav-btn > a').toggleClass('is-open');
+    $('.p-nav__btn > a').toggleClass('is-open');
 });
