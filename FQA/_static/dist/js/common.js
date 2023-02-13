6adccbd10a557ab85cecd6aa12275a6112d3362a
unknown <flaming.june123@gmail.com>
2023-02-13 22:14:53 +0900

    20230213_2214

--- FQA/_static/dist/js/common.js
+++ FQA/_static/dist/js/common.js
@@ -1,51 +1,10 @@
-//crossを押したときに、Aを表示させる
-const target = document.querySelectorAll(".c-cross");
-const sectionAnswer = document.querySelector(".p-fqa__answer");
-
-//とりあえず親要素を取得する。
-//親要素の取得も失敗しているので、clickイベントと同様for分を使わなければならないのではないか？
-const parent = target.parentNode;
-console.log(parent);
-
-//parentの親要素を取得する
-
-//const nextSibling = parent.nextElementSibling;
-
-//console.log(nextSibling);
+//十字click時のイベント
 
 document.querySelectorAll(".c-cross").forEach(function(element){
   element.addEventListener('click',function(){
       this.classList.toggle("is-active");
-      //nextSibling.classList.toggle("is-active");
+      //c-crossの親要素の次の要素
+      const answerOpen = this.parentNode.nextElementSibling;
+      answerOpen.classList.toggle("is-active");
   });
 });
-
-document.querySelectorAll(".c-cross").forEach(function(element){
-  element.addEventListener('click',function(){
-      this.classList.toggle("is-active");
-      //nextSibling.classList.toggle("is-active");
-  });
-});
-
-//親要素までさかのぼって、そこから兄弟要素に行く方法は？
-
-
-
-/*
-参考コード
-document.querySelectorAll('.cards').forEach(function(cards){
-
-  cards.addEventListener('click',function(){
-      console.log(cards);
-
-  });
-});
-*/
-
-/*
-target.addEventListener("click", () => {
-    target.forEach((element) => {
-      element.classList.toggle("is-active");
-  });
-})
-*/
