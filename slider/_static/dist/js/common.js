6aa2e952c86e9cb0db152b8c20ec76644d5e7d32
unknown <flaming.june123@gmail.com>
2023-02-20 21:04:29 +0900

    20230220_2104

--- slider/_static/dist/js/common.js
+++ slider/_static/dist/js/common.js
@@ -1,3 +1,28 @@
+// slick 2週目のカクつき対応
+const $slider = $(".js-slider");
+
+$slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+	$slider.find(".slick-slide").each((index, el) => {
+		const $this = $(el),
+		slickindex = $this.attr("data-slick-index");
+		if (nextSlide == slick.slideCount - 1 && currentSlide == 0) {
+			if (slickindex == "-1") {
+				$this.addClass("is-active-next");
+			} else {
+				$this.removeClass("is-active-next");
+			}
+		} else if (nextSlide == 0) {
+			if (slickindex == slick.slideCount) {
+				$this.addClass("is-active-next");
+			} else {
+				$this.removeClass("is-active-next");
+			}
+		} else {
+			$this.removeClass("is-active-next");
+		}
+	});
+});
+
 /*slider */
 $('.slick01').slick({
   autoplay: true, //「オプション名: 値」の形式で書く
@@ -7,6 +32,7 @@ $('.slick01').slick({
   //オートプレイ
   infinite: true,
   autoplaySpeed: 1,
+  
   speed: 5000,
   cssEase: "linear",
   //slickが途中で止まるのを防ぐ
@@ -22,3 +48,4 @@ $('.slick01').slick({
       },
     ],
 });
+
