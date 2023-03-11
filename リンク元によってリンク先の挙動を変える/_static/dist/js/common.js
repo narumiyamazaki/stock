7c73ca65e9f4b5fc5a7464e9cc30c6eeaefc3836
unknown <flaming.june123@gmail.com>
2023-03-11 18:48:31 +0900

    20230311_1848

--- リンク元によってリンク先の挙動を変える/_static/dist/js/common.js
+++ リンク元によってリンク先の挙動を変える/_static/dist/js/common.js
@@ -1,23 +1,46 @@
-const linkToSource = document.querySelectorAll('.p-link').forEach{
-
-}
+const linkToSource = document.querySelectorAll('.p-link');
 
 
-const linkTarget = document.querySelectorAll('.p-link-item__text');
+linkToSource.forEach(function(linkToSource){
+  linkToSource.addEventListener('click', function(e){
+    //linkの何番目かでクエリに渡す値を変える
+    if( this.dataset.linkNumber == 1 ){
+      location.href="./to_link.html?name=" +  encodeURIComponent(1);
+    } else if(this.dataset.linkNumber == 2) {
+      location.href="./to_link.html?name=" +  encodeURIComponent(2);
+    } else if(this.dataset.linkNumber == 3) {
+      location.href="./to_link.html?name=" +  encodeURIComponent(3);
+    } else if(this.dataset.linkNumber == 4) {
+      location.href="./to_link.html?name=" +  encodeURIComponent(4);
+    } else if(this.dataset.linkNumber == 5) {
+      location.href="./to_link.html?name=" +  encodeURIComponent(5);
+    }
+  })
+});
 
-console.log(linkToSource);
+const linkTarget = document.querySelectorAll('.link-target');
+
+//console.log(linkToSource);
+
+
+
 
 //クリックされたときのターゲットを調べ、それにより条件分岐
 
-linkToSource.addEventListener('click',function(event){
-  console.log('hoge');
-});
+var query = location.search;
+var value = query.split('=');
 
-/*
-//addEventListenerのコールバック関数
-function serchLink(){/
-  if( this === linkToSource[0]){
-    linkTarget.classList.add('is-active');
+  if(decodeURIComponent(value[1]) == 1){
+    linkTarget[0].classList.add('is-active');
+  } else if(decodeURIComponent(value[1]) == 2) {
+    linkTarget[1].classList.add('is-active');
+  } else if(decodeURIComponent(value[1]) == 3) {
+    linkTarget[2].classList.add('is-active');
+  } else if(decodeURIComponent(value[1]) == 4) {
+    linkTarget[3].classList.add('is-active');
+  } else if(decodeURIComponent(value[1]) == 5) {
+    linkTarget[4].classList.add('is-active');
   }
-}
-*/
+
+
+console.log(decodeURIComponent(value[1]));
