a1e5a368511443cf75981e0e2649296767a020ea
前進主義芝原28 <flaming.june123@gmail.com>
2023-10-30 17:27:56 +0900

    20231030_1727

--- header/header-list1/_static/dist/js/on.click.js
+++ header/header-list1/_static/dist/js/on.click.js
@@ -1,9 +1,20 @@
-/*ハンバーガーメニューのクリックイベント*/
+const navBtn = document.querySelector(".p-nav__btn");
+const navSp = document.querySelector(".p-header__nav__body--sp");
+const humbergerLine = document.querySelector(".p-humberger__line")
+
+navBtn.addEventListener('click',() => {
+    navBtn.classList.toggle('open');
+    navSp.classList.toggle('open');
+    humbergerLine.classList.toggle('open');
+},false);
+
+/*ハンバーガーメニューのクリックイベント
 $('.p-nav__btn').on('click', function () {
     $(this).toggleClass('open');
     $('.p-header__nav__body--sp').toggleClass('open');
     $('.p-nav__btn > a').toggleClass('open');
 });
+*/
 
 /*サブメニューのクリックイベント*/
 $('.c-icon--has-sub-menu').on('click', function () {
