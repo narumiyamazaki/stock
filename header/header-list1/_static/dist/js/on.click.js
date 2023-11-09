c60fafe765e936a6593b0fe5d2d9cc7eb3f8fc8c
前進主義芝原28 <flaming.june123@gmail.com>
2023-11-09 16:47:38 +0900

    20231109_1647

--- header/header-list1/_static/dist/js/on.click.js
+++ header/header-list1/_static/dist/js/on.click.js
@@ -1,11 +1,62 @@
 const navBtn = document.querySelector(".p-nav__btn");
 const navSp = document.querySelector(".p-header__nav__body--sp");
-const humbergerLine = document.querySelector(".p-humberger__line")
+const humbergerLine = document.querySelector(".p-humberger__line");
+let flg = false;
+
+const backgroundFix = (bool) => {
+    const scrollingElement = () => {
+      const browser = window.navigator.userAgent.toLowerCase();
+      if ("scrollingElement" in document) return document.scrollingElement;
+      return document.documentElement;
+    };
+  
+  let scrollY;
+  
+  if (bool) {
+    scrollY = scrollingElement().scrollTop;
+  } else {
+    const bodyStyleTop = parseInt(document.body.style.top || "0");
+    if (isNaN(bodyStyleTop)) {
+      scrollY = 0;
+    } else {
+      scrollY = bodyStyleTop;
+    }
+  }
+  
+    const fixedStyles = {
+      height: "100vh",
+      position: "fixed",
+      top: `${scrollY * -1}px`,
+      left: "0",
+      width: "100vw"
+    };
+  
+    Object.keys(fixedStyles).forEach((key) => {
+    if (bool) {
+      document.body.style[key] = fixedStyles[key];
+    } else {
+      document.body.style[key] = "";
+    }
+  });
+  
+    if (!bool) {
+      window.scrollTo(0, scrollY * -1);
+    }
+  };
 
 navBtn.addEventListener('click',() => {
     navBtn.classList.toggle('open');
     navSp.classList.toggle('open');
     humbergerLine.classList.toggle('open');
+    if(flg){
+        backgroundFix(false);
+        navBtn.setAttribute("aria-expanded", "false");
+        flg = false;
+    }else{
+        backgroundFix(true);
+        navBtn.setAttribute("aria-expanded", "true");
+        flg = true;
+    }
 },false);
 
 //escキー押下でハンバーガーメニューを閉じられるように
@@ -16,6 +67,7 @@ window.addEventListener("keydown", () => { if (event.key === "Escape") {
     humbergerLine.classList.remove('open');
   }
 });
+
 
 /*サブメニューのクリックイベント*/
 $('.c-icon--has-sub-menu').on('click', function () {
