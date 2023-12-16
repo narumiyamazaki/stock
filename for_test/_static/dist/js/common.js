00eff092d8e7290298e2d18961099d2aefc7562c
前進主義芝原28 <flaming.june123@gmail.com>
2023-12-16 10:58:33 +0900

    20231216_1058

---
+++ for_test/_static/dist/js/common.js
@@ -0,0 +1,53 @@
+/*トップに戻る*/
+$(function () {
+    var pagetop = $('#js-pagetop');
+    pagetop.hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({
+          scrollTop: 0
+      }, 500);
+      return false;
+    });
+  });
+
+  /*slider
+  $('.slick01').slick({
+    autoplay: true, //「オプション名: 値」の形式で書く
+    centerMode: true,
+    centerPadding: '20%',
+    arrows: false,
+    //オートプレイ
+    infinite: true,
+    autoplaySpeed: 0,
+    speed: 9000,
+    cssEase: "linear",
+    responsive: [
+        {
+          breakpoint: 400, // 399px以下のサイズに適用
+          settings: {
+          slidesToShow: 1,
+          },
+        },
+      ],
+  });
+ */
+
+  $(function () {
+    $(".fade").on("inview", function () {
+      $(this).addClass("fade-in");
+    });
+  });
+  
+
+  $(function () {
+    $(".CMS-NEWS-ITEM").on("inview", function () {
+      $(this).addClass("fade-in");
+    });
+  });
