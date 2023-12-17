56df494b592e69a27dcdd4e437f868aa119bba26
unknown <flaming.june123@gmail.com>
2023-12-17 20:02:31 +0900

    20231217_2002

--- for_test/_static/dist/js/common.js
+++ for_test/_static/dist/js/common.js
@@ -1,4 +1,4 @@
-/*トップに戻る*/
+/*トップに戻る
 $(function () {
     var pagetop = $('#js-pagetop');
     pagetop.hide();
@@ -16,38 +16,75 @@ $(function () {
       return false;
     });
   });
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  const pagetop = document.querySelector('#js-pagetop');
+  pagetop.style.display = 'none';
 
-  /*slider
-  $('.slick01').slick({
-    autoplay: true, //「オプション名: 値」の形式で書く
-    centerMode: true,
-    centerPadding: '20%',
-    arrows: false,
-    //オートプレイ
-    infinite: true,
-    autoplaySpeed: 0,
-    speed: 9000,
-    cssEase: "linear",
-    responsive: [
-        {
-          breakpoint: 400, // 399px以下のサイズに適用
-          settings: {
-          slidesToShow: 1,
-          },
-        },
-      ],
+  window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > 500) {
+      fadeIn(pagetop);
+    } else {
+      fadeOut(pagetop);
+    }
   });
- */
 
-  $(function () {
-    $(".fade").on("inview", function () {
-      $(this).addClass("fade-in");
-    });
+  pagetop.addEventListener('click', function () {
+    scrollToTop();
+    return false;
   });
-  
+});
 
-  $(function () {
-    $(".CMS-NEWS-ITEM").on("inview", function () {
-      $(this).addClass("fade-in");
-    });
-  });
+function fadeIn(element) {
+  element.style.display = 'block';
+
+  (function fade() {
+    let val = parseFloat(element.style.opacity) || 0;
+    
+    if (val < 1) {
+      val += 0.05; // Adjust the step as needed
+      element.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function fadeOut(element) {
+  element.style.opacity = 1;
+
+  (function fade() {
+    if ((element.style.opacity -= 0.1) < 0) { // Adjust the step as needed
+      element.style.display = 'none';
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function scrollToTop() {
+  let startTime;
+  let duration = 500;
+  let startOffset = document.documentElement.scrollTop;
+
+  function animateScroll(currentTime) {
+    if (!startTime) {
+      startTime = currentTime;
+    }
+
+    let progress = currentTime - startTime;
+    let easeInOutQuad;
+    if (progress / (duration / 2) < 1) {
+      easeInOutQuad = 0.5 * Math.pow(progress / (duration / 2), 2);
+    } else {
+      easeInOutQuad = 1 - 0.5 * Math.abs(Math.pow((progress - duration) / (duration / 2), 2));
+    }
+
+    document.documentElement.scrollTop = easeInOutQuad * (0 - startOffset) + startOffset;
+
+    if (progress < duration) {
+      requestAnimationFrame(animateScroll);
+    }
+  }
+
+  requestAnimationFrame(animateScroll);
+}
